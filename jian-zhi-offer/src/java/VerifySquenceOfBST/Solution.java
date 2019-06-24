87537f630a49f0793db522f3876db0e11cd2a684
lichun <lichun@abcaikedeMac-Pro.local>
2019-06-24 15:18:17 +0800

    bst

---
+++ jian-zhi-offer/src/java/VerifySquenceOfBST/Solution.java
@@ -0,0 +1,22 @@
+package VerifySquenceOfBST;
+
+public class Solution {
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        if (sequence == null || sequence.length == 0) return false;
+        return validate(sequence, 0, sequence.length - 1);
+    }
+
+    private boolean validate(int[] s, int left, int right){
+        if (left >= right) return true;
+        int cur = right;
+        while(cur > left && s[cur - 1] > s[right]) cur--;
+        for (int i = left; i < cur - 1; i++) {
+            if (s[i] >= s[right]) return false;
+        }
+        return validate(s, left, cur - 1) && validate(s, cur, right - 1);
+    }
+
+    public static void main(String [] args) {
+    }
+}
+
