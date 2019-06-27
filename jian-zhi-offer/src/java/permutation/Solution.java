cb6679b176d71d2104a8e7ff7b81f57eb081de89
lichun <lichun@abcaikedeMac-Pro.local>
2019-06-27 13:16:57 +0800

    permutation

--- jian-zhi-offer/src/java/permutation/Solution.java
+++ jian-zhi-offer/src/java/permutation/Solution.java
@@ -1,4 +1,33 @@
 package permutation;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+
 public class Solution {
+    public ArrayList<String> Permutation(String str) {
+        if (str == null || str.isEmpty()) return new ArrayList<>();
+        HashSet<String> r = new HashSet<>();
+        permutation(r, str.toCharArray(), 0);
+        ArrayList<String> result = new ArrayList<>(r);
+        result.sort(Comparator.naturalOrder());
+        return new ArrayList<String>(result);
+    }
+
+    private void permutation(HashSet r, char[] str, int start) {
+        if (start == str.length) {
+            r.add(new String(str));
+        }
+        for (int i = start; i < str.length; i++) {
+            swap(str, i, start);
+            permutation(r, str, start + 1);
+            swap(str, start, i);
+        }
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+    }
 }
