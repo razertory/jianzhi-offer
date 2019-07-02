b9941f5a5bfade763b0cf9ded09a218ebc3f8228
Razertory <razertory@163.com>
2019-07-02 08:33:11 +0800

    change name to dp

--- src/main/java/jianzhioffer/greatestsubarray/Solution1.java
+++ src/main/java/jianzhioffer/greatestsubarray/Solution1.java
@@ -9,12 +9,12 @@ public class Solution1 {
 
     public int FindGreatestSumOfSubArray(int[] array) {
         if (array == null || array.length == 0) return 0;
-        int[] sum = new int[array.length];
+        int[] dp = new int[array.length];
         int result = array[0];
-        sum[0] = result;
+        dp[0] = result;
         for (int i = 1; i < array.length; i++) {
-            sum[i] = Math.max(sum[i - 1] + array[i], array[i]);
-            result = Math.max(sum[i], result);
+            dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
+            result = Math.max(dp[i], result);
         }
         return result;
     }
