b7269e583507c911c051d747a60f6bcc3834a9fe
Razertory <razertory@163.com>
2019-07-10 21:24:38 +0800

    增加三道题

--- src/main/java/jianzhioffer/multiply/Solution.java
+++ src/main/java/jianzhioffer/multiply/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
         int[] B = new int[A.length];
         B[0] = 1;
         for (int i = 1; i < A.length; i++) {
-            B[i] = A[i - 1] + B[i - 1];
+            B[i] = A[i - 1] * B[i - 1];
         }
         int temp = 1;
         for(int j = A.length-2; j >= 0; j--){
